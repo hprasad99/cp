7dcb99fe3fa37826fcb28551b28db6e6fafdd0f8
himanshuprasad <chatwith.himanshu27@gmail.com>
2026-01-19 22:02:34 +0530

    2176A cf operation with inversions

--- codeforces/2176A.cpp
+++ codeforces/2176A.cpp
@@ -2,7 +2,28 @@
  * https://codeforces.com/problemset/problem/2176/A
  */
 #include<iostream>
+#include<algorithm>
 using namespace std;
+int solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i=0; i<n; i++) {
+        cin >> a[i];
+    }
+    int mx=0, cmx=0;
+    for(int i=0; i<n; i++) {
+        mx = max(mx, a[i]);
+        if(mx == a[i]) cmx++;
+    }
+    int res = n - cmx;
+    return res;
+}
 int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        cout << solve() << endl;
+    }
     return 0;
 }
