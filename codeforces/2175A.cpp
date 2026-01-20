936343c54ee1e212bc3eefe75ec97533f8c89c1e
himanshuprasad <chatwith.himanshu27@gmail.com>
2026-01-20 23:16:57 +0530

    2175A - Little Fairy's Painting solution

---
+++ codeforces/2175A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <climits>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for(int i=0; i<n; i++) {
+            cin >> a[i];
+        }
+
+        set<int> st(a.begin(), a.end());
+        int d = st.size(); 
+
+        int ans = INT_MAX;
+        for(int x: a) {
+            if(x >= d) {
+                ans = min(ans, x);
+            }
+        }
+        
+        cout<<ans<<'\n';
+    }
+    return 0;
+}
