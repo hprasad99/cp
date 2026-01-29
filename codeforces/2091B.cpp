48d0f6266f16a627a52486ad13a361da5a2e2076
himanshuprasad <chatwith.himanshu27@gmail.com>
2026-01-30 00:03:36 +0530

    B. Team Training solution

---
+++ codeforces/2091B.cpp
@@ -0,0 +1,33 @@
+/**
+ * team training n students ith student -> ai
+ * 
+ */
+#include<iostream>
+#include<algorithm>
+using namespace std;
+void solve() {
+    int n, x;
+    cin >> n >> x;
+    int a[n];
+    for(int i=0; i<n; i++) {
+        cin >> a[i];
+    }
+    sort(a, a+n);
+    reverse(a, a+n);
+    int ans = 0;
+    for(int i=0, cnt=1; i<n; i++, cnt++){
+        if(a[i] * cnt >= x) {
+            ans++;
+            cnt = 0;
+        }
+    }
+    cout<< ans <<endl;
+}
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0; 
+}
